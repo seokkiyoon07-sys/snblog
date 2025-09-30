cf19881a54e8bea150b036a7e9b1b391dfd1752f
Seokki Yoon <seokkiyoon07@gmail.com>
2025-09-30 11:58:15 +0900

    Reorder SN Originals content by date (newest first)
    - Move 관동별곡 (Dec 19) to top
    - Move 북천가 (Dec 20) to second
    - Move 화왕가 (Dec 21) to third
    - Move 갑민가 (Dec 22) to bottom (most recent)
    - Maintain chronological order with newest content at top

--- src/app/originals/page.tsx
+++ src/app/originals/page.tsx
@@ -2,28 +2,17 @@ export default function OriginalsPage() {
   const originals = [
     {
       id: 1,
-      title: '내 속도 모르면서 가라 마라 막말하지 마라 (수능 고전문학 「갑민가」)',
-      excerpt: 'AI 그림과 친절한 해설로 즐기는 고전문학! 보기만 해도 국어 감각이 살아납니다. 수능 대비는 물론, 고전문학을 친숙하게 이해하고 싶다면 지금 바로 시작하세요!',
-      date: 'Dec 22, 2024',
-      readTime: '8 min read',
-      tags: ['SN Originals', '고전문학', '수능국어', '갑민가'],
-      thumbnail: 'https://img.youtube.com/vi/3ZGE-RvBinQ/maxresdefault.jpg',
+      title: '정철의 강원도 브이로그 (고전문학 「관동별곡」)',
+      excerpt: '정철의 관동별곡을 현대적 브이로그 형식으로 재해석! 강원도의 아름다운 자연과 정철의 감정을 생생하게 느껴보세요. 수능 고전문학의 대표작을 재미있게 배우는 특별한 시간입니다.',
+      date: 'Dec 19, 2024',
+      readTime: '9 min read',
+      tags: ['SN Originals', '고전문학', '수능국어', '관동별곡'],
+      thumbnail: 'https://img.youtube.com/vi/9T06DoJaMHs/maxresdefault.jpg',
       category: 'SN Originals',
-      youtubeUrl: 'https://www.youtube.com/watch?v=3ZGE-RvBinQ'
+      youtubeUrl: 'https://www.youtube.com/watch?v=9T06DoJaMHs'
     },
     {
       id: 2,
-      title: '너 그렇게 여자만 좋아하다 피똥싼다 (수능 고전문학 「화왕가」)',
-      excerpt: '화왕가는 조선 후기 작가 김만중의 작품으로, 당시 사회의 모순과 부조리를 풍자한 고전문학입니다. 현대적 해석과 함께 수능에 자주 출제되는 핵심 내용을 재미있게 배워보세요!',
-      date: 'Dec 21, 2024',
-      readTime: '7 min read',
-      tags: ['SN Originals', '고전문학', '수능국어', '화왕가'],
-      thumbnail: 'https://img.youtube.com/vi/6tsl5IIMy_o/maxresdefault.jpg',
-      category: 'SN Originals',
-      youtubeUrl: 'https://www.youtube.com/watch?v=6tsl5IIMy_o&t=2s'
-    },
-    {
-      id: 3,
       title: '좋긴 한데 유배 생활 이래도 됨? 고전문학 「북천가」',
       excerpt: '북천가는 조선 후기 문인들의 유배 생활을 그린 작품으로, 자연에 대한 사랑과 정치적 현실에 대한 아쉬움을 담고 있습니다. 수능 고전문학의 핵심 작품을 쉽고 재미있게 이해해보세요!',
       date: 'Dec 20, 2024',
@@ -34,15 +23,26 @@ export default function OriginalsPage() {
       youtubeUrl: 'https://www.youtube.com/watch?v=xhyppBm3_o4'
     },
     {
-      id: 4,
-      title: '정철의 강원도 브이로그 (고전문학 「관동별곡」)',
-      excerpt: '정철의 관동별곡을 현대적 브이로그 형식으로 재해석! 강원도의 아름다운 자연과 정철의 감정을 생생하게 느껴보세요. 수능 고전문학의 대표작을 재미있게 배우는 특별한 시간입니다.',
-      date: 'Dec 19, 2024',
-      readTime: '9 min read',
-      tags: ['SN Originals', '고전문학', '수능국어', '관동별곡'],
-      thumbnail: 'https://img.youtube.com/vi/9T06DoJaMHs/maxresdefault.jpg',
+      id: 3,
+      title: '너 그렇게 여자만 좋아하다 피똥싼다 (수능 고전문학 「화왕가」)',
+      excerpt: '화왕가는 조선 후기 작가 김만중의 작품으로, 당시 사회의 모순과 부조리를 풍자한 고전문학입니다. 현대적 해석과 함께 수능에 자주 출제되는 핵심 내용을 재미있게 배워보세요!',
+      date: 'Dec 21, 2024',
+      readTime: '7 min read',
+      tags: ['SN Originals', '고전문학', '수능국어', '화왕가'],
+      thumbnail: 'https://img.youtube.com/vi/6tsl5IIMy_o/maxresdefault.jpg',
       category: 'SN Originals',
-      youtubeUrl: 'https://www.youtube.com/watch?v=9T06DoJaMHs'
+      youtubeUrl: 'https://www.youtube.com/watch?v=6tsl5IIMy_o&t=2s'
+    },
+    {
+      id: 4,
+      title: '내 속도 모르면서 가라 마라 막말하지 마라 (수능 고전문학 「갑민가」)',
+      excerpt: 'AI 그림과 친절한 해설로 즐기는 고전문학! 보기만 해도 국어 감각이 살아납니다. 수능 대비는 물론, 고전문학을 친숙하게 이해하고 싶다면 지금 바로 시작하세요!',
+      date: 'Dec 22, 2024',
+      readTime: '8 min read',
+      tags: ['SN Originals', '고전문학', '수능국어', '갑민가'],
+      thumbnail: 'https://img.youtube.com/vi/3ZGE-RvBinQ/maxresdefault.jpg',
+      category: 'SN Originals',
+      youtubeUrl: 'https://www.youtube.com/watch?v=3ZGE-RvBinQ'
     },
   ];
 
