3272e4e87e82f3e7f9f2a65d76261c1c85270a1a
SN Academy <admin@snacademy.co.kr>
2025-10-03 22:19:24 +0900

    test: add test component to debug import issues

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export function TestComponent() {
+  return (
+    <div className="bg-red-500 text-white p-4 rounded">
+      <h2>Test Component Works!</h2>
+      <p>This is a test component to verify imports are working.</p>
+    </div>
+  );
+}
